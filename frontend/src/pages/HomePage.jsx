683641f5d23be85d12d7a075693abec3461e88bd
LuanCalais <luancalais@gmail.com>
2025-04-23 13:27:09 -0300

    fix: style and delete

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,67 @@
+import { Container, SimpleGrid, Text, VStack } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useProductStore } from "../store/product";
+import ProductCard from "../components/ProductCard";
+
 export default function HomePage() {
-    return <h1>Home Page Works</h1>;
-  }
-  
+  const { getProducts, products } = useProductStore();
+
+  useEffect(() => {
+    getProducts();
+  }, [getProducts]);
+
+  return (
+    <Container maxW="container.xl" py={12}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={"30"}
+          fontWeight={"bold"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+          textAlign={"Center"}
+        >
+          Current Products
+        </Text>
+
+        {products.length ? (
+          <SimpleGrid
+            columns={{
+              base: 1,
+              md: 2,
+              lg: 3,
+            }}
+            spacing={10}
+            width={"full"}
+          >
+            {products.map((product, i) => (
+              <ProductCard
+                key={`${product.id}_${Math.random()}_${i}`}
+                product={product}
+              />
+            ))}
+          </SimpleGrid>
+        ) : (
+          <Text
+            fontSize={"xl"}
+            fontWeight={"bold"}
+            bgClip={"text"}
+            textAlign={"Center"}
+            bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          >
+            No products{" :( "} <br />
+            <Link to="/create">
+              <Text
+                as={"span"}
+                color={"blue.500"}
+                _hover={{ textDecoration: "underline" }}
+              >
+                Create Product
+              </Text>
+            </Link>
+          </Text>
+        )}
+      </VStack>
+    </Container>
+  );
+}
--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -22,6 +22,24 @@ export const useProductStore = create((set) => ({
         );
         return { success: true, message: 'Product created successfully' }
 
+    },
+
+    getProducts: async () => {
+        const res = await fetch("/api/products")
+        const data = await res.json()
+        set({ products: data.data })
+    },
+
+    deleteProduct: async (id) => {
+        const res = await fetch(`/api/products/${id}`, {
+            method: 'DELETE'
+        })
+        const data = await res.json()
+        if (!data.success) return { success: false, message: data.message }
+        set(state => ({ products: state.products.filter(product => product._id !== id) }))
+        return {
+            success: true, message: data.message
+        }
     }
 }))
 
